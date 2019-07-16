e48a42dbcf199ab356fa2759b8961b0009e1a019
Demon988 <51952262+Demon988@users.noreply.github.com>
2019-07-16 15:20:41 +0700

    Follow Camera + Raycast Camera

---
+++ Assets/Scripts/PlayerScripts/CameraFollow.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraFollow : MonoBehaviour
+{
+    private GameObject Player;
+    void Start()
+    {
+        Player = GameObject.FindGameObjectWithTag("Player");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        this.transform.position = Player.transform.position;
+    }
+}
