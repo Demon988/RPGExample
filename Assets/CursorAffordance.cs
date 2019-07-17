81278bfe83a558061e1e6d5f9dd2f1dffb34c97c
Demon988 <51952262+Demon988@users.noreply.github.com>
2019-07-17 15:38:52 +0700

    click to move + update cursor

---
+++ Assets/CursorAffordance.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CursorAffordance : MonoBehaviour
+{
+    [SerializeField] Texture2D WalkCursor = null;
+    [SerializeField] Texture2D AttackCursor = null;
+    [SerializeField] Texture2D UnknowCursor = null;
+    [SerializeField] Vector2 CursorHotport = new Vector2(24,24);
+    CameraRaycaster CameraRaycaster;
+    void Start()
+    {
+        CameraRaycaster = Camera.main.GetComponent<CameraRaycaster>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        switch (CameraRaycaster.layerHit)
+        {
+            case Layer.Walkable:
+                Cursor.SetCursor(WalkCursor, CursorHotport, CursorMode.Auto);
+                break;
+            case Layer.Enemy:
+                Cursor.SetCursor(AttackCursor, CursorHotport, CursorMode.Auto);
+                break;
+            default:
+                Cursor.SetCursor(UnknowCursor, CursorHotport, CursorMode.Auto);
+                return;
+        }
+    }
+}
---
+++ Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityStandardAssets.Characters.ThirdPerson;
+
+public class PlayerMovement : MonoBehaviour
+{
+    [SerializeField] float WalkStopPoint = 0.2f;
+    CameraRaycaster CameraRaycaster;
+    private Vector3 PlayerPosition;
+    ThirdPersonCharacter m_thirdPersonCharacter;
+    void Start()
+    {
+        CameraRaycaster = Camera.main.GetComponent<CameraRaycaster>();
+        PlayerPosition = transform.position;
+        m_thirdPersonCharacter = GetComponent<ThirdPersonCharacter>();
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        if (Input.GetMouseButton(0))
+        {
+            switch (CameraRaycaster.layerHit)
+            {
+                case Layer.Walkable:
+                    PlayerPosition = CameraRaycaster.hit.point;
+                    break;
+            }
+        }
+        var postion = PlayerPosition - transform.position;
+        if (postion.magnitude >= WalkStopPoint)
+        {
+            m_thirdPersonCharacter.Move(postion, false, false);
+        }
+        else
+        {
+            m_thirdPersonCharacter.Move(Vector3.zero, false, false);
+
+        }
+    }
+}
